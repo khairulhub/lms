b72ac308047076eff297022b983f80407672dd99
khairulhub <21103033@iubat.edu>
2024-05-01 18:16:13 +0600

    user migration is updated for multple user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('address')->nullable();
+            $table->enum('role',['admin','instructor','user'])->default('user');
+            $table->enum('status',['1','0'])->default('1')->comment('1 for active and 0 for inactive');
             $table->rememberToken();
             $table->timestamps();
         });
