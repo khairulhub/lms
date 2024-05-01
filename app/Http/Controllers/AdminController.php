eed8a4a5f50e0c6ff0f1f83095e4d5c49f308905
khairulhub <21103033@iubat.edu>
2024-05-02 00:33:44 +0600

    admin dashboard + theme setup + logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,19 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-
+//admin routes group
 Route::middleware(['auth','role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
 
-Route::middleware(['auth'])->group(function () {
+
+//instructor routes group
+Route::middleware(['auth','role:instructor'])->group(function () {
+
     Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
 });
+
+
+
